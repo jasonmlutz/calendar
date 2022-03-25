bfd27b88330068a178756a2ebe0fe95a5542e1d3
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-25 12:56:04 -0700

    Add simple render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
 
 test("renders calendar app title", () => {
   render(<App />);
