b24518a8b2f77dafd22eb5e4d2beabfb139fe2bd
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-25 14:44:31 -0700

    Build and test generateMonth

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { generateMonth } from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -12,4 +12,9 @@ test("renders calendar app title", () => {
   render(<App />);
   const linkElement = screen.getByText(/calendar app/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("generateMonth: properly assigns month", () => {
+  expect(generateMonth(0)).toEqual({ index: 0, name: "January" });
+  expect(generateMonth(8)).toEqual({ index: 8, name: "September" });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
 import React from "react";
 
+type MonthIndex = Readonly<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11>;
+
+type MonthName = Readonly<
+  | "January"
+  | "February"
+  | "March"
+  | "April"
+  | "May"
+  | "June"
+  | "July"
+  | "August"
+  | "September"
+  | "October"
+  | "November"
+  | "December"
+>;
+
+interface Month {
+  readonly index: MonthIndex;
+  readonly name: MonthName;
+}
+
+export function generateMonth(index: MonthIndex): Month {
+  // take an index in 0 .. 11 and return the corresponding
+  // Month object
+  const monthNames: MonthName[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return {
+    index: index,
+    name: monthNames[index],
+  };
+}
+
 function App() {
   return (
     <div id="App" className="container mx-auto text-white">
