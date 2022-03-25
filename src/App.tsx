d9a26e68bf0cde6740d2d371d17d9f3ee0b9c086
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-25 11:36:49 -0700

    Style and clear App
    Remove App.css, clear App.tsx and replace with skeleton.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="App" className="container mx-auto text-white">
+      <header id="App-header" className="py-2 text-center text-2xl md:text-3xl">
+        Calendar App
       </header>
     </div>
   );
