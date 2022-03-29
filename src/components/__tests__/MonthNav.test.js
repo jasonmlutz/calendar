32d1202db78181f7ec2fcb413ea025bc4af69757
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-29 12:54:27 -0700

    Write left & right nav button tests

--- src/components/__tests__/MonthNav.test.js
+++ src/components/__tests__/MonthNav.test.js
@@ -26,3 +26,31 @@ it("renders initial state correctly", () => {
   });
   expect(container.textContent).toBe("January");
 });
+
+it("correctly increments to next month", () => {
+  act(() => {
+    render(<MonthDisplay />, container);
+  });
+  // grab the right-nav button
+  const rightButton = document.querySelector("#Current-month-right-nav > svg");
+  expect(container.textContent).toBe("January");
+  act(() => {
+    rightButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(container.textContent).toBe("February");
+});
+
+it("correctly increments to previous month", () => {
+  act(() => {
+    render(<MonthDisplay />, container);
+  });
+  // grab the left-nav button
+  const leftButton = document.querySelector("#Current-month-left-nav > svg");
+  expect(container.textContent).toBe("January");
+  act(() => {
+    leftButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(container.textContent).toBe("December");
+});
